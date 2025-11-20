55ca447f31a356cd189b800ede44f510d39845f5
irbis-bim <beltikova.v@irbiscompany.ru>
2025-11-20 18:19:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 
-APPTITLE = os.getenv("APP_TITLE", "YouGile Sync")
 APITOKEN = os.getenv("YOUGILE_API_TOKEN", "")
 PGDSN = os.getenv("DATABASE_URL", "")
 SCHEMA = os.getenv("PG_SCHEMA", "public")
-HEADLESS = os.getenv("HEADLESS", "1") == "1"
